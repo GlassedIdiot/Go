10112fbe6258b653e8220521d49e9b08a5ee688c
GlassedIdiot <rehmanareeb69@gmail.com>
2024-06-26 01:03:34 +0500

    Helper functions.

--- Chapter-5/subdomains/sub_domain.go
+++ Chapter-5/subdomains/sub_domain.go
@@ -4,11 +4,49 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	"github.com/miekg/dns"
 )
 
 type Result struct {
 	HostName  string `json:"hostname"`
 	IPaddress string `json:"ipadderess"`
+}
+
+func A_Name(fqdn, dns_server string) ([]string, error) {
+	var dns_msg dns.Msg
+	var ip_addr []string
+
+	dns_msg.SetQuestion(dns.Fqdn(fqdn), dns.TypeA)
+	response, err := dns.Exchange(&dns_msg, "1.1.1.1:53")
+	if err != nil {
+		return ip_addr, err
+	}
+
+	for _, answer := range response.Answer {
+		if a, ok := answer.(*dns.A); ok {
+			ip_addr = append(ip_addr, a.A.String())
+		}
+	}
+	return ip_addr, nil
+}
+
+func CName(fqdn, dns_server string) ([]string, error) {
+	var dns_msg dns.Msg
+	var ip_addr []string
+
+	dns_msg.SetQuestion(dns.Fqdn(fqdn), dns.TypeCNAME)
+	response, err := dns.Exchange(&dns_msg, "1.1.1.1:53")
+	if err != nil {
+		return ip_addr, err
+	}
+
+	for _, answer := range response.Answer {
+		if a, ok := answer.(*dns.CNAME); ok {
+			ip_addr = append(ip_addr, a.Target)
+		}
+	}
+	return ip_addr, nil
 }
 
 func main() {
